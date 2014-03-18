8b0e6ea404dc32479a47372f7d97ae87e4af7634
Kimpe Andy <andykimpe@gmail.com>
2014-03-18 12:25:09 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 wget http://download-euro.oldapps.com/IDM/idman505.exe
 echo "Please close idm after install"
 echo "For this just click on the icon on the top right idm then on exit"
+sleep 20
 WINEPREFIX="$HOME/.idm" WINARCH=win32 wine idman505.exe
 sudo mkdir -p /opt/idm
 sudo cp -R $HOME/.idm/* /opt/idm
@@ -9,14 +10,15 @@ sudo wget https://raw.github.com/andykimpe/idmforlinux/master/idman -q -O /usr/b
 sudo chmod +x /usr/bin/idman
 echo "install flashgot"
 echo "Please restart firefox after install"
+sleep 20
 sudo rm -f /usr/local/sbin/install-mozilla-addon /usr/sbin/install-mozilla-addon
 sudo wget -q -O /usr/sbin/install-mozilla-addon https://raw.github.com/andykimpe/idmforlinux/master/install-mozilla-addon
 sudo chmod +x /usr/sbin/install-mozilla-addon
 sudo mkdir -p /usr/local/sbin/
 sudo ln -s /usr/sbin/install-mozilla-addon /usr/local/sbin/install-mozilla-addon
-install-mozilla-addon https://addons.mozilla.org/firefox/downloads/latest/220/addon-220-latest.xpi > /dev/null 2>&1
-killall firefox > /dev/null 2>&1
-killall firefox-trunk > /dev/null 2>&1
+install-mozilla-addon https://addons.mozilla.org/firefox/downloads/latest/220/addon-220-latest.xpi
+killall firefox
+killall firefox-trunk > /dev/null
 echo "IDM finish install"
 echo "for ingrate idm on flashgot"
 echo "Please use /usr/bin/idman"
